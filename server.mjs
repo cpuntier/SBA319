b70532cd134ab0afde516415f1a48458099e36e8
cpuntier <cpuntier@gmail.com>
2024-01-26 20:14:10 -0500

    seed route added

--- server.mjs
+++ server.mjs
@@ -13,7 +13,36 @@ app.use(express.json());
 // //    req.movies = await db.collection('movies');
 //     console.log("movies awaited")
 // })
-
+app.get("/seed", async (req, res) => {
+    let collection = await db.collection('testcollection');
+    try {
+      // These methods should work for both, MongoDB Native Driver OR Mongoose
+      // This will delete everything in the collection before the insetMany is ran,
+      // this is done to prevent the database from getting cluttered. 
+      // Below I'm using the Model like I would do in Mongoose but if you're using the Native Driver it'll be something like this (await collection.deleteMany({}) 
+      await collection.deleteMany({}) 
+      await collection.insertMany([
+        {
+          name: "Superman",
+          powers: ["Flying", "Super Strength", "Laser Vision"],
+          weakness: "Kryptonite"
+        },
+        {
+          name: "Spiderman",
+          powers: ["Web Slinging", "Super Strength", "Spidey Senses"],
+          weakness: "Pizza"
+        },
+        {
+          name: "Iron Man",
+          powers: ["Flying", "Bulletproof", "Laser Blasters"],
+          weakness: "Infinity Gauntlet"
+        }
+      ])
+      res.send("OK")
+    } catch (err) {
+      res.status(500).send("Something went wrong.")
+    }
+  })
 app.use("/characters", router);
 
 app.get("/",async (req,res) => {
