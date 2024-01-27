d0ed0a4e842004fc5f1078d7935e32986fbce14a
cpuntier <cpuntier@gmail.com>
2024-01-27 16:02:50 -0500

    get name added

--- routes/characters.mjs
+++ routes/characters.mjs
@@ -9,19 +9,14 @@ router.route('/')
         let characters = await collection.find().toArray();
         console.log(characters);
         res.send(characters)
-    
+    })
 
-        // if (req.query["rating"]) {
-        //     const character = characters.filter((c) => c.avgRate > req.query.rating)
-        //     if (character.length == 0) {
-        //         res.send("No characters found")
-        //     } else {
-        //         res.json(character)
-        //     }
-        // } else {
-
-        //     res.json(characters);
-        // }
+router.route('/:name') //matches name of character gives you information on that character. Names are case sensitive
+    .get( async (req,res,next) => {
+        let collection = await db.collection('characters');
+        let characters = await collection.find({name:req.params.name}).toArray();
+        console.log(characters);
+        res.send(characters);
     })
 
 
