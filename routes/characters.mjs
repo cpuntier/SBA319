86fb5e1d5ec5f2369bdd606cda0ac15ef1fa1a68
cpuntier <cpuntier@gmail.com>
2024-01-27 17:26:04 -0500

    post char works

--- routes/characters.mjs
+++ routes/characters.mjs
@@ -5,11 +5,31 @@ import db from "../db/conn.mjs";
 
 router.route('/')
     .get(async (req, res) => {
+
         let collection = await db.collection('characters');
         let characters = await collection.find().toArray();
         console.log(characters);
         res.send(characters)
-    })
+        }).post(async (req,res,next) => {
+            let collection = await db.collection('characters');
+            try {
+    
+                const post = await collection.insertOne({
+                    name : req.body.name,
+                    description: req.body.description,
+                    ratings : req.body.ratings,
+                    img_src: req.body.img_src
+                })
+                res.send(post);
+            } catch (error) {
+                res.send(error);
+                
+            }
+        })
+    
+
+
+
 
 router.route('/:name') //matches name of character gives you information on that character. Names are case sensitive
     .get( async (req,res,next) => {
@@ -18,6 +38,10 @@ router.route('/:name') //matches name of character gives you information on that
         console.log(characters);
         res.send(characters);
     })
+
+
+
+
 
 
 export default router;
